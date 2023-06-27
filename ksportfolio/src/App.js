09fc3c1671da88d62378822cd222e184a5d0b6a4
kshwi <kshwiss@gmail.com>
2023-06-27 10:15:41 -0500

    learn it but not today

--- ksportfolio/src/App.js
+++ ksportfolio/src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, but not today
         </a>
       </header>
     </div>
