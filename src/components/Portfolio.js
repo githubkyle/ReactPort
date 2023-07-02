60cbb3a91c0ea17b3fb81775007fdb894305e19c
kshwi <kshwiss@gmail.com>
2023-07-02 16:48:23 -0500

    got the links and images to display correctly

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,40 +4,40 @@ import Project from "./pages/project";
 const projects = [
   {
     title: "Server Notes",
-    imageUrl: "../servernotes.png",
+    imageUrl: "servernotes.png",
     GithubRepo: "https://github.com/githubkyle/ServerNotes",
     DeployedPage: "https://githubkyle.github.io/ServerNotes/"
   },
   {
     title: "Weather Dashboard",
-    imageUrl: "../weather.png",
+    imageUrl: "weather.png",
     GithubRepo: "https://github.com/githubkyle/Weather-Dashboard",
     DeployedPage: "https://githubkyle.github.io/Weather-Dashboard/"
   },
   {
     title: "JavaScript Quiz",
-    imageUrl: "../jsq.png",
+    imageUrl: "jsq.png",
     GithubRepo: "https://github.com/githubkyle/Javascript-Quiz",
     DeployedPage: "https://githubkyle.github.io/Javascript-Quiz/"
   },
 
   {
     title: "Daily Planner",
-    imageUrl: "../planner.png",
+    imageUrl: "planner.png",
     GithubRepo: "https://github.com/githubkyle/Planner",
     DeployedPage: "https://githubkyle.github.io/Planner/"
   },
 
   {
     title: "Password Generator",
-    imageUrl: "../password.png",
+    imageUrl: "password.png",
     GithubRepo: "https://github.com/githubkyle/Password-Generator",
     DeployedPage: "https://githubkyle.github.io/Password-Generator/"
   },
 
   {
     title: "First Portfolio",
-    imageUrl: "../firstport.png",
+    imageUrl: "firstport.png",
     GithubRepo: "https://github.com/githubkyle/Challenge2",
     DeployedPage: "https://githubkyle.github.io/Challenge2/"
   }
@@ -51,8 +51,8 @@ const Portfolio = () => {
           key={index}
           title={project.title}
           imageUrl={project.imageUrl}
-          githubRepo={project.GithubRepo}
-          deployment={project.DeployedPage}
+          GithubRepo={project.GithubRepo}
+          DeployedPage={project.DeployedPage}
         />
       ))}
     </div>
--- src/components/pages/project.js
+++ src/components/pages/project.js
@@ -4,9 +4,17 @@ const Project = ({ title, imageUrl, GithubRepo, DeployedPage }) => {
   return (
     <div className="project">
       <h3>{title}</h3>
-      <img src={imageUrl} alt={title} />
-      <h2>{GithubRepo}</h2>
-      <h2>{DeployedPage}</h2>
+      <img
+        src={require(`../../assets/${imageUrl}`)}
+        alt={title}
+        style={{ maxWidth: "450px", height: "500px" }}
+      />
+      <a href={GithubRepo}>
+        <h3>GitHub Repo</h3>
+      </a>
+      <a href={DeployedPage}>
+        <h3>Deployed Page</h3>
+      </a>
     </div>
   );
 };
