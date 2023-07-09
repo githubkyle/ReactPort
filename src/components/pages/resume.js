5f706e7a369369058158b4361551c250ca4e3f79
kshwi <kshwiss@gmail.com>
2023-07-08 19:23:04 -0500

    fixed the pictures displayed after tutor help

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -5,8 +5,8 @@ export default function Resume() {
     <div>
       <h1>Kyle's Resume</h1>
       <p>
-        <a>
-          https://gist.github.com/githubkyle/61bf782ed75ce13e0bb7d264edce2a99
+        <a href="https://gist.github.com/githubkyle/61bf782ed75ce13e0bb7d264edce2a99">
+          My Resume
         </a>
       </p>
       <div>Hire me?</div>
