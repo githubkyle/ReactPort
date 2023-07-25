186c30dc51c775e63aa62b70739b4a495ef4c5a3
kshwi <kshwiss@gmail.com>
2023-07-24 20:22:12 -0500

    edited the content of app.js css so the footer doesn't interfere with clickable content

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div>
       <h1>About Page</h1>
-      <img src="../../assets/self.jpg" alt="self"></img>
+      <img src="self.jpg" alt="self"></img>
       <p>
         Kyle Schwent was born and raised in STL. He's played chess most of his
         life, is interested in languages, cats and playing sports.
